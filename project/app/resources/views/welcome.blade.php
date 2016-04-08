cdf74a1b4d551224cae8efb08b576f29e47c37cd
Felipe Diep <fdiep@blim.com>
2016-04-08 10:07:03 -0400

    Add travis env and configuration

--- project/app/resources/views/welcome.blade.php
+++ project/app/resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <div class="ui raised very padded text container center aligned segment">
-    <h2 class="ui middle header">Laravel 5</h2>
+    <h2 class="ui middle header">Laravel 5 - Semantic UI &amp; Angular</h2>
     <p></p>
     <p></p>
   </div>
